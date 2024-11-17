c80ef2999086155158a124250a0ab8cd2b613d37
abulseco <ashley.bulseco@gmail.com>
2024-11-17 07:51:12 -0500

    feat: added 16S tutorial file

---
+++ 16S-DADA2-Tutorial.R
@@ -0,0 +1,15 @@
+# Moore Lab 16S DADA2 Tutorial
+# November 18, 2024
+# Last Updated: ANB on 11/18/24
+
+# Setup your environment----
+# This tutorial assumes you have downloaded the appropriate practice data
+library(dada2); library(dplyr)
+
+# Run the following if you need to install dada2
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("dada2")
+
+
+
